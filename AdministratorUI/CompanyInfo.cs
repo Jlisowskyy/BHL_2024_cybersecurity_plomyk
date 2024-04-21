b723c9f72b8b712808f87892fb055b4f4f96bfb4
Jakub Lisowski <115403674+Jlisowskyy@users.noreply.github.com>
2024-04-21 05:23:13 +0200

    Added ui files to the repo

---
+++ AdministratorUI/CompanyInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackatonUI
+{
+    public class CompanyInfo
+    {
+        // Temporary list of depatments, will be replaced with database
+        public readonly List<string> departmentNames = new List<string>()
+        {
+            "Human Resources",
+            "Finance and Accounting",
+            "Marketing",
+            "Sales",
+            "Research and Development",
+            "Customer Service",
+            "Information Technology (IT)",
+            "Operations",
+            "Administration",
+            "Legal and Compliance",
+            "Supply Chain Management",
+            "Product Management",
+            "Quality Assurance",
+            "Public Relations",
+            "Corporate Communications"
+        };
+
+        // Test types list
+        public readonly List<string> TestTypes = new List<string>()
+        {
+            "Attack from the outside",
+            "Attack from the inside"
+        };
+
+        // Test media list
+        public readonly List<string> TestMedia = new List<string>()
+        {
+            "email",
+            "sms"
+        };
+    }
+}
---
+++ AdministratorUI/TestConfigs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HackatonUI
+{
+    public class TestConfigs
+    {
+        // Base test interval
+        public decimal BaseTestInterval { get; set; }
+
+        // Points for reporting
+        public decimal ReportPoints { get; set; }
+
+        // Points for ignoring
+        public decimal IgnorePoints { get; set; }
+
+        // Points for failing
+        public decimal PenaltyPoints { get; set; }
+
+        // Points limit, users which exceeded the limit shall retake the course
+        public decimal PointsThreshold { get; set; }
+
+        // List of departments names
+        public List<string> Departments { get; set; }
+
+        // List of test types
+        public List<string> TestTypes { get; set; }
+
+        // List of media types
+        public List<string> MediaTypes { get; set; }
+
+        [JsonConstructor]
+        public TestConfigs(decimal baseTestInterval, decimal reportPoints, 
+            decimal ignorePoints, decimal penaltyPoints, decimal pointsThreshold, 
+            List<string> departments, List<string> testTypes, List<string> mediaTypes) 
+        {
+            BaseTestInterval = baseTestInterval;
+            ReportPoints = reportPoints;
+            IgnorePoints = ignorePoints;
+            PenaltyPoints = penaltyPoints;
+            PointsThreshold = pointsThreshold;
+            Departments = departments;
+            TestTypes = testTypes;
+            MediaTypes = mediaTypes;
+        }
+    }
+}
