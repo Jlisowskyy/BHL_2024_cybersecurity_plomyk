09bcf05519e54826ae14733a0d7dcd1ed42c2359
Jakub Lisowski <115403674+Jlisowskyy@users.noreply.github.com>
2024-04-21 06:57:03 +0200

    Tested build&run

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 python -m venv .venv
 ./.venv/Scripts/activate
 pip install -r ConnectionSolution/requirements.txt
-pip install -r WebScrapperSolution/requirements.txt
+pip install -r WebScraperSolution/requirements.txt
 cd AdministratorUI
 dotnet build
 cd ..
