e682c5084c2c0c606d78e26a483a2052c6aa72d8
Michal <kwiatkowksi.michal.2003@gmail.com>
2024-04-21 03:35:25 +0200

    basic worker dataset

--- ConnectionSolution/prepareBasicWorkerSet.py
+++ ConnectionSolution/prepareBasicWorkerSet.py
@@ -1,8 +1,18 @@
+import time
+
 import MainFlowLib as fLib
 
 def getTestSet() -> [fLib.Worker]:
     rv = list()
 
-    rv.append(fLib.Worker("jason", "darulo", "Jlisowskyysnk@gmail.com", [fLib.LinkedinData("https://www.linkedin.com/in/john-smith-78b294305/")]))
+    rv.append(fLib.Worker("Jason", "Darulo", "Jlisowskyysnk@gmail.com",
+                          [fLib.LinkedinData("https://www.linkedin.com/in/john-smith-78b294305/")],
+                          lastTest=time.time()))
+    rv.append(fLib.Worker("Cristiano", "Ronaldo", "Jlisowskyysnk@gmail.com",
+                          [fLib.LinkedinData("https://www.linkedin.com/in/john-smith-78b294305/")],
+                          lastTest=time.time()))
+    rv.append(fLib.Worker("Ronaldinho", "Gaucho", "Jlisowskyysnk@gmail.com",
+                          [fLib.LinkedinData("https://www.linkedin.com/in/john-smith-78b294305/")],
+                          lastTest=time.time()))
 
     return rv
